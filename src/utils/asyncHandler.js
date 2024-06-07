62efa9e260ff96cc0cbcfee44e9bfdd1571717e3
Kashif Mohd <kashifmohd901@gmail.com>
2024-06-07 12:00:32 -0500

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+export { asyncHandler };
+
+// const asyncHander = () => {};
+// const asyncHander = (func) => () => {};
+// const asyncHander = (func) => async () => {};
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
